685a589f89879c5de175f455b0a676d282319922
Marc Robison <marc@marcrobison.com>
2025-07-28 14:03:14 +0200

    fix: refresh graph every 30s
    If no date is selected then current date will be refreshed by default,
    thus updating the current date to the following day.
    
    Fixes #1

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def create_figure(df):
             dict(type="rect", xref="paper", yref="y",
                  x0=0, x1=1, y0=0, y1=70,
                  fillcolor="rgba(255,0,0,0.1)", line_width=0),
-
+    
             # Red zone - high
             dict(type="rect", xref="paper", yref="y",
                  x0=0, x1=1, y0=200, y1=300,
@@ -46,7 +46,8 @@ def create_figure(df):
 
 fig = create_figure(df)
 
-app = Dash(__name__, requests_pathname_prefix="/glucose/")
+#app = Dash(__name__, requests_pathname_prefix="/glucose/")
+app = Dash(__name__, url_base_pathname="/glucose/")
 app.layout = html.Div([
     html.H1('Glucose'),
     dcc.Graph(id="glucose-graph",figure=fig),
@@ -63,33 +64,40 @@ app.layout = html.Div([
             html.P(id='err', style={'color': 'red'})
         ],
         style={'textAlign': 'center'},
+    ),
+    dcc.Interval(
+        id='interval-component',
+        interval=10*1000, # update every 10s
+        n_intervals=0
     )
 ])
 
 @callback(
         Output('glucose-graph', 'figure'),
         Output('err', 'children'),
+        Input('interval-component', 'n_intervals'),
         Input(component_id='date-picker', component_property='date'))
-def update_glucose_graph_from_date(date):
+def update_glucose_graph_from_date(n, date):
+    if date:
+        con_cb = sqlite3.connect("my_db")  
+        query = """
+        SELECT * FROM glucose
+        WHERE date(timestamp) = ?
+        """
+        df_cb = pd.read_sql_query(query, con_cb, params=(date,))
+        con_cb.close()
 
-    con_cb = sqlite3.connect("my_db")  
-    query = """
-    SELECT * FROM glucose
-    WHERE date(timestamp) = ?
-    """
-    df_cb = pd.read_sql_query(query, con_cb, params=(date,))
-    con_cb.close()
+        if df_cb.empty:
+            #raise PreventUpdate
+            date_obj = datetime.strptime(date, '%Y-%m-%d')
+            return no_update, 'No data available for {}'.format(date_obj.strftime('%d-%m-%Y'))
 
-    if df_cb.empty:
-        #raise PreventUpdate
-        date_obj = datetime.strptime(date, '%Y-%m-%d')
-        return no_update, 'No data available for {}'.format(date_obj.strftime('%d-%m-%Y'))
-
+        else:
+            return create_figure(df_cb), ''
     else:
+        con_cb = sqlite3.connect("my_db")  
+        df_cb = pd.read_sql_query("select timestamp, value from glucose WHERE date(timestamp) = date('now');", con_cb)
         return create_figure(df_cb), ''
 
-#if __name__ == '__main__':
-#    app.run(debug=True)
-
-server = app.server  # IMPORTANT: expose the Flask server to Gunicorn
-
+if __name__ == '__main__':
+    app.run(debug=True)
